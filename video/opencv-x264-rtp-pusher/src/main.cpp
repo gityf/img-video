307efbdf6ad0538b4cf9837634584189002dc932
Yaofu <voipman@sina.cn>
2023-08-21 18:26:03 +0800

    Update main.cpp

--- video/opencv-x264-rtp-pusher/src/main.cpp
+++ video/opencv-x264-rtp-pusher/src/main.cpp
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 2018 Wang Yaofu
+** Copyright (C) 2023 Wang Yaofu
 ** All rights reserved.
 **
 **Author:Wang Yaofu voipman@qq.com
@@ -20,20 +20,23 @@ const static int kFrameFPS = 25;
 
 #define STREAM_PUSH_INS Singleton<StreamPush>::Instance()
 
-int initPush(const char* addr, int port) {
+int initPush(const char *addr, int port, int codec) {
     EndPointMgr *endPointMgr = new EndPointMgr();
     endPointMgr->addEndPoint(addr, port);
 
-    RtpStream* rtpStream = new RtpStream();
+    RtpStream *rtpStream = new RtpStream();
 
     rtpStream->SetEndPointMgr(endPointMgr);
     STREAM_PUSH_INS->init(kFrameWidth, kFrameHeight, kFrameChannel, kFrameFPS);
     STREAM_PUSH_INS->setRtpStream(rtpStream);
     STREAM_PUSH_INS->setRtpEnable(true);
+    if (codec == 265) {
+        STREAM_PUSH_INS->useCodec(AV_CODEC_ID_H265);
+    }
     return 0;
 }
 
-void handleVideo(const char* pFileName) {
+void handleVideo(const char *pFileName) {
     Mat frame;
     cv::VideoCapture capture(pFileName);
 
@@ -42,18 +45,34 @@ void handleVideo(const char* pFileName) {
         if (frame.empty()) {
             break;
         }
-
         STREAM_PUSH_INS->push(&frame);
         usleep(20000);
     }
 }
 
-int main(int argc, char* argv[]) {
+void handleCameraVideo(int cameraId) {
+    Mat frame;
+    cv::VideoCapture capture(cameraId);
+    capture.set(cv::CAP_PROP_FRAME_WIDTH, kFrameWidth);
+    capture.set(cv::CAP_PROP_FRAME_HEIGHT, kFrameHeight);
+
+    while (true) {
+        capture >> frame;
+        if (frame.empty()) {
+            break;
+        }
+        //printf("w:%d,h:%d\n", frame.cols, frame.rows);
+        STREAM_PUSH_INS->push(&frame);
+        usleep(20000);
+    }
+}
+
+int main(int argc, char *argv[]) {
     if (argc == 4) {
-        initPush(argv[1], atoi(argv[2]));
-        handleVideo(argv[3]);
+        initPush(argv[1], atoi(argv[2]), atoi(argv[3]));
+        handleCameraVideo(0);
     } else {
-        printf("Usage: ip sport video-file-name\n");
+        printf("Usage: ip sport 265/264\n");
     }
     return 0;
 }
